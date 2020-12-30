db208b84d4d40945956f824cfebad528459feb1c
Igor <75262127+Mazepin@users.noreply.github.com>
2020-12-30 23:41:01 +0300

    init: add program Draw Rectangle, Triangles and InvertedTriangle

---
+++ TestPr15 DrawTriangles/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace TestPr15_DrawTriangles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // рисуем прямоугольный треугольник, затем такой же на убывание
+
+            Console.Write("Введите высоту треугольника: ");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+
+            // рисуем перевернутые треугольники
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestPr16 DrawRectangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TestPr16_DrawRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // рисуем прямоугольник
+
+            Console.Write("Введите высоту прямоугольника: ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите ширину треуголника: ");
+            int widht = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < widht; j++)
+                {
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
