2d61d308e8e2bda00e98b1dee155d0e5d0734e58
Igor <75262127+Mazepin@users.noreply.github.com>
2020-12-30 15:44:50 +0300

    init: add program Remove Digit From Number

---
+++ TestPr9 RemoveDigitFromNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TestPr9_RemoveDigitFromNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // remove a digit from number
+
+            int result1 = 0, number2 = 1;
+            Console.WriteLine("Enter you number:");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the digit to be removed from the number :");
+            int digit = int.Parse(Console.ReadLine());
+
+            while (number != 0)
+            {
+                int result = number % 10;
+                number = number / 10;
+                if (result == digit)
+                    continue;
+
+                result1 = result1 + result * number2;
+                number2 = number2 * 10;
+            }
+
+            Console.WriteLine($"The number without {digit} is: ");
+            Console.WriteLine(result1);
+            Console.ReadKey();
+        }
+    }
+}
