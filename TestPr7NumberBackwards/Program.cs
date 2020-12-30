ade1f91497946e0726c26efdd76387707b5b457b
Igor <75262127+Mazepin@users.noreply.github.com>
2020-12-30 15:36:53 +0300

    init: add program number backwards

---
+++ TestPr7NumberBackwards/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TestPr7NumberBackwards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // сделать число перевертышь
+
+            int number, result;
+            Console.WriteLine("Enter you number:");
+            number = int.Parse(Console.ReadLine());
+            
+            while (number > 0)
+            {
+                result = result + number % 10;
+                number = number / 10;
+            }
+            
+            Console.WriteLine("Your inverted number : ");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
