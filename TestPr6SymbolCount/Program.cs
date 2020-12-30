752da5f194e12e9b0adaf08b528ae9b14670be9a
Igor <75262127+Mazepin@users.noreply.github.com>
2020-12-30 15:22:13 +0300

    init: add program symbol count via method

---
+++ TestPr6SymbolCount/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TestPr6SymbolCount
+{
+    class Program
+    {
+        // вывод символа введенного пользователем, столько раз, сколько захочет пользователь. Вывод через метод
+        static void PrintString(string symbol, uint symbolCount)
+        {
+            for (int i = 0; i < symbolCount; i++)
+            {
+                Console.Write(symbol);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символ: ");
+            string symbolUser = Console.ReadLine();
+
+            Console.WriteLine("Введите количество символов: ");
+            uint symbolCountUser = uint.Parse(Console.ReadLine());
+
+            Console.WriteLine("=========================");
+            PrintString(symbolUser, symbolCountUser);            // вызываем метод PrintString
+            Console.ReadLine();
+        }
+    }
+}
