54f5250d00f78d34602ea19a22611d174c3e8fe1
Igor <75262127+Mazepin@users.noreply.github.com>
2020-12-30 15:40:56 +0300

    init: add program Output value by index

---
+++ TestPr8OutputValueByIndex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TestPr8OutputValueByIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // вывод значения из числа по введенному индексу
+
+            Console.WriteLine("Enter you number : ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter you index of number : ");
+            int index = int.Parse(Console.ReadLine());
+            index -= 1; // если понятно, что отчисление начинается с 0, то эта строка не нужна
+
+            string result = number.ToString();
+
+            Console.WriteLine("The index of the number corresponds to the digit :");
+            Console.WriteLine(result[index]);
+            Console.ReadKey();
+        }
+    }
+}
