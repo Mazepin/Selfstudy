b15693d9bd17a4fa6f15d0fab8d408977f7d3789
Igor <75262127+Mazepin@users.noreply.github.com>
2021-01-27 16:43:26 +0300

    init: add outputting the sum of array elements using recursion

---
+++ TestPr24.1 Рекурсия/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TestPr24._1_Рекурсия
+{
+    class Program
+    {
+        // Найти сумму элементов массива с помощью рекурсии
+
+        static int Sum(int[] myArray, int i = 0)
+        {
+            if (i >= myArray.Length)
+            {
+                return 0;
+            }
+
+            int result = Sum(myArray, i + 1);  // рекурсия, вызываем метод в методе
+
+            return myArray[i] + result;  // обязательный выход из рекурсии
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 356, 2, 77 };
+            int result = Sum(myArray);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
