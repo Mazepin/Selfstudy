c3c4eb5ce8bcb82d19b8fe06b9b80ed6bff7286c
Igor <75262127+Mazepin@users.noreply.github.com>
2021-01-27 16:07:55 +0300

    init: add outputting an array using recursion

---
+++ TestPr24 Рекурсия/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TestPr24_Рекурсия
+{
+    class Program
+    {
+
+        // Вывод массива с помощью рекурсии
+
+        static void PrintArray(int[] myArray, int i = 0)
+        {
+            if (i < myArray.Length)
+            {
+                Console.WriteLine(myArray[i]);
+                PrintArray(myArray, i + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 2, 5, 3 };
+            PrintArray(myArray);
+            Console.ReadKey();
+        }
+    }
+}
