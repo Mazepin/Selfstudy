d43a21090c94927fbc290bd63205d7781eb121de
Igor <75262127+Mazepin@users.noreply.github.com>
2021-02-12 21:19:48 +0300

    add: enum, definition of the key

---
+++ TestPr25.1 Enum Part1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TestPr25._1_Enum_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // definition of the key that was pressed on the keyboard
+
+            while (true)
+            {
+                Console.WriteLine("\nPress any key \nif you press the key ENTER, the program will exit");
+
+                ConsoleKey key = Console.ReadKey().Key; // enum console
+
+                int keyKode = (int)key;
+
+                Console.WriteLine($"\tEnum key:  {key} \tKey code: {keyKode}");
+
+                if (key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine("You pressed Enter!");
+                    return;
+                }
+            }
+        }
+    }
+}
