89a07eda35d4ba29d60025b81663aa80f17f0bbb
Igor <75262127+Mazepin@users.noreply.github.com>
2021-02-13 21:54:05 +0300

    add: Create dll

---
+++ TestPr27 dll - creating a class library/MyLibrary/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class Student
+    {
+        public Guid ID;
+        public string firstName;
+        public string lastName;
+        public string middleName;
+        public int age;
+        public string group;    
+    }
+}
---
+++ TestPr27 dll - creating a class library/TestPr27 dll - creating a class library/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using MyLibrary;
+
+namespace TestPr27_dll___creating_a_class_library
+{
+    class Program
+    {
+        static Student GetStudent()
+        {
+            Student student = new Student();
+
+            student.firstName = "Inna";
+            student.lastName = "Mazepina";
+            student.middleName = "Victorovna";
+            student.age = 33;
+            student.ID = Guid.NewGuid();
+            student.group = "TMS06";
+
+            return student;
+        }
+
+        static void Print(Student student)
+        {
+            Console.WriteLine("Information about student");
+            Console.WriteLine($"ID: {student.ID}");
+            Console.WriteLine($"Name: {student.firstName}");
+            Console.WriteLine($"Surname: {student.lastName}");
+            Console.WriteLine($"Patronymic: {student.middleName}");
+            Console.WriteLine($"Age: {student.age}");
+            Console.WriteLine($"Group: {student.group}");
+        }
+
+        static void Main(string[] args)
+        {
+            var firstPerson = GetStudent();
+
+            Print(firstPerson);
+
+            Console.ReadKey();
+
+        }
+    }
+}
