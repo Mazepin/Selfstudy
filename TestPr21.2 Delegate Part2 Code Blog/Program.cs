f823aac696ffdf26486a313eae15465b739b3b89
Igor <75262127+Mazepin@users.noreply.github.com>
2021-02-16 00:07:00 +0300

    init: add Delegate Action, Predicate & Func

---
+++ TestPr21.2 Delegate Part2 Code Blog/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace TestPr21._2_Delegate_Part2_Code_Blog
+{
+
+
+    public delegate void MyDelegate(); // объявляем делегат
+    class Program
+    {
+        public delegate int ValueDelegate(int i);  // объявляем делегат
+        static void Main(string[] args)
+        {
+
+            // 1-ый способ присвоения и вызова делегата
+
+            MyDelegate myDelegate = Method1;   // присваеваем делегату имя метода1
+            myDelegate(); // делегат вызывает метод1
+            Console.WriteLine("==========");
+            myDelegate += Method4;  // добавляем к делегату еще один метод !!!
+            myDelegate += Method1; // добавляем еще один метод. Делегат вызывает 3 метода сразу последовательно
+            myDelegate();
+            Console.WriteLine("==========");
+
+            // 2-ой способ вызова и присвоения делегата
+
+            MyDelegate myDelegate2 = new MyDelegate(Method1);  // вызов конструктора и в качестве параметра передаем имя метода1
+            myDelegate2 += Method4; // добавляем к делегату еще один метод4
+            myDelegate2.Invoke();
+            Console.WriteLine("==========");
+            myDelegate2 -= Method4; // удаляем из делегата метод4
+            myDelegate2.Invoke();
+            Console.WriteLine("==========");
+            myDelegate2 = myDelegate + myDelegate2;  // к делегату2 добавляем еще один делегат1
+            myDelegate2.Invoke();   // вызываем делегат который вызывает метод1
+            Console.WriteLine("==========");
+
+            // 3-ий пример вызова и присвоения делегата
+
+            var valueDelegate = new ValueDelegate(ValueMethod);
+            valueDelegate += ValueMethod;  // одно число будет передано во все эти методы ниже!!!
+            valueDelegate += ValueMethod;  
+            valueDelegate += ValueMethod;
+            valueDelegate += ValueMethod;
+
+            valueDelegate((new Random()).Next(1000));   // вызываем делегат и передаем в него рандомное число
+            Console.WriteLine("==========");
+            valueDelegate(13);  // присваеваем 13 методу ValueMethod через вызов делегат
+
+
+            Console.ReadLine();
+        }
+
+        public static int ValueMethod(int i)
+        {
+            Console.WriteLine(i);
+            return i;
+        }
+
+        static void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+
+        static int Method2()
+        {
+            Console.WriteLine("Method2");
+            return 0;
+        }
+
+        static void Method3(int i)
+        {
+            Console.WriteLine("Method3");
+        }
+
+        static void Method4()
+        {
+            Console.WriteLine("Method4");
+        }
+    }
+}
+
---
+++ TestPr21.3 Delegate Action, Predicate and Func/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TestPr21._3_Delegate_Action__Predicate_and_Func
+{
+    class Program
+    {
+        // Delegate Action, Predicate & Func
+
+        //public delegate void Action(); // можно не объявлять вот так, т.к. Action это уже подразумевает
+        // объявили встроенную (шаблонную) делегату Action, которая не возвращает нам значения
+
+        static void Main(string[] args)
+        {
+       // ACTION
+
+            Action action = Method1; // объявляем делегат и присваеваем ему значение метода1. Выше эту делегату можно не объявлять!!!
+            action();
+
+            Action<int> action1;   // СООТВЕТСТВУЕТ вот этому встроенному объявлению public delegate void Action (int i)
+            Action<int, int, string> action2;  // СООТВЕТСТВУЕТ public delegate Action (int, int, string)
+            Console.WriteLine("===========");
+
+
+       // PREDICATE
+
+            Predicate<object> predicate1;  //  СООТВЕТСТВУЕТ объявлению public delegate bool Predicate(int value);
+                                           // в качестве возвращаемого значения true или false 
+                                           // используется для сравнения, сопоставления некоторого объекта object определенному условию
+            
+            Predicate<int> isPositive = delegate (int x) { return x > 0; };
+
+            Console.WriteLine(isPositive(20));  // TRUE
+            Console.WriteLine(isPositive(-20));  // FALSE
+            Console.WriteLine("===========");
+
+       // FUNC
+
+            Func<string, char, int> function1;  // СООТВЕТСТВУЕТ public delegate int Func(string str, char c)
+                                          // ПОСЛЕДНИЙ параметр int - это мы задаем тип возвращаемого значения
+                                          // далее до 16 штук типы аргументов, может их не быть
+
+
+
+            Console.ReadLine();
+        }
+
+        static void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+        static int Method2()
+        {
+            Console.WriteLine("Method2");
+            return 0;
+        }
+    }
+}
