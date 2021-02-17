3467d5c4bd253e18a9b22d55373df2c9cab47ba7
Igor <75262127+Mazepin@users.noreply.github.com>
2021-02-17 10:54:00 +0300

    add: delegate Event

---
+++ TestPr21.4 Delegate Event/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestPr21._4_Delegate_Event
+{
+    class Person
+    {
+        public event Action GoToSleep;
+
+        public event EventHandler NotSleepNow;  // объявляем дополнительное событие
+        public string NamePerson { get; set; }
+
+        public void TakeTime(DateTime now)
+        {
+            if (now.Hour <= 8)
+            {
+                GoToSleep?.Invoke();
+            }
+            else
+            {
+                var args = new EventArgs(); // создаем дополнительный параметр и потом передаем его в дополнительное событие NotSleepNow
+                NotSleepNow?.Invoke(this, args);  // передаем тот объект который был вызван
+            }
+        }
+    }
+}
---
+++ TestPr21.4 Delegate Event/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TestPr21._4_Delegate_Event
+{
+    class Program
+    {
+        // Delegate Event, EventHandler
+
+        public delegate int DelegateValue(int i);
+
+        public delegate void MyDelegate();
+
+        public event MyDelegate DelegateEvent; // объявляем событие
+
+        public event Action EventeAction; // это событие совпадает с тем что выше
+
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            Console.WriteLine("Entet a name of Person");
+            person.NamePerson = Console.ReadLine();
+
+            person.GoToSleep += Person_GoToSleep; // присвоили событие GoToSleep 
+            person.NotSleepNow += Person_NotSleepNow; // подписываемся на событие NotSleepNow
+
+
+            person.TakeTime(DateTime.Now);
+            //person.TakeTime(DateTime.Parse("16-02-2021 19:24:07"));
+            //person.TakeTime(DateTime.Parse("16-02-2021 4:24:07"));
+
+            Console.ReadLine();
+        }
+
+        private static void Person_NotSleepNow(object sender, EventArgs e)
+        {
+            if (sender is Person)   // выполняем проверку от неправильного ввода
+            {
+                Console.WriteLine($"{((Person)sender).NamePerson} does not sleep now !!!"); // приводим object к типу person
+            }
+        }
+
+        private static void Person_GoToSleep()
+        {
+            Console.WriteLine("Person go to sleep !!!");
+        }
+    }
+}
