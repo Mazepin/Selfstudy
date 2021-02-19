20c74eb5c5d15b30e71bdca3e3e08c7d05504bd6
Igor <75262127+Mazepin@users.noreply.github.com>
2021-02-19 22:50:03 +0300

    init: add OOP Test Encapsulation

---
+++ TestPr26.5 OOP (object oriented programming) Part5 Encapsulation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TestPr26._5_OOP__object_oriented_programming__Part5_Encapsulation
+{
+    class Program
+    {
+        // OOP Part5 Encapsulation
+
+        class Gun
+        {
+            private bool isLoaded; // обязательное условие для выстрела, поэтому ставим private и инкапсулируем
+
+            private void ReLoad()  // инкапсулируем 
+            {
+                Console.WriteLine("Loading weapon...");
+
+                isLoaded = true;
+
+                Console.WriteLine("The gun is loaded");
+            }
+
+            public void Shoot()
+            {
+                if (!isLoaded)
+                {
+                    Console.WriteLine("The gun is not loaded...");
+                    ReLoad();
+                }
+                Console.WriteLine("Shot!!!");
+                isLoaded = false;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Gun gun = new Gun();
+            gun.Shoot();
+
+            Console.ReadKey();
+        }
+    }
+}
