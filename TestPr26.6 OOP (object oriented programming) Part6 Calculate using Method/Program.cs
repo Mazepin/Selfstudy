78afe5eb4c35b23f478a5668762e63d23a62a27e
Igor <75262127+Mazepin@users.noreply.github.com>
2021-02-21 00:23:36 +0300

    ii

---
+++ TestPr26.6 OOP (object oriented programming) Part6 Calculate using Method/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace TestPr26._6_OOP__object_oriented_programming__Part6_Calculate_using_Method
+{
+    class Program
+    {
+        // Calculate (using Method)
+
+        static double Addition(double a, double b)
+        {
+            double result = a + b;
+            Console.WriteLine($"{a} + {b} = {result}");
+
+            return result;
+        }
+        static double Subtraction(double a, double b)
+        {
+            double result = a - b;
+            Console.WriteLine($"{a} - {b} = {result}");
+
+            return result;
+        }
+
+        static double Multiply(double a, double b)
+        {
+            double result = a * b;
+            Console.WriteLine($"{a} * {b} = {result}");
+
+            return result;
+        }
+
+        static double Division(double a, double b)
+        {
+            if (b != 0)
+            {
+                double result = a / b;
+                Console.WriteLine($"{a} / {b} = {result}");
+            }
+            else
+            {
+                Console.WriteLine("Error!!! Division by zero");
+            }
+            return a / b;
+        }
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("==============");
+                Console.WriteLine("\nEnter first number: ");
+                string number1 = Console.ReadLine();
+                double _number1;
+                bool a = double.TryParse(number1, out _number1);
+
+                Console.WriteLine("Enter arithmetic operation ");
+                string operation = Console.ReadLine();
+
+                Console.WriteLine("Enter second number: ");
+                // double _number2 = double.Parse(Console.ReadLine()); // можно так запарсить
+
+                string number2 = Console.ReadLine();
+                double _number2;
+                bool b = double.TryParse(number2, out _number2);
+
+                switch (operation)
+                {
+                    case "+":
+                        Addition(_number1, _number2);
+                        break;
+                    case "-":
+                        Subtraction(_number1, _number2);
+                        break;
+                    case "*":
+                        Multiply(_number1, _number2);
+                        break;
+                    case "/":
+                        Division(_number1, _number2);
+                        break;
+                    default:
+                        Console.WriteLine("\nIncorrect input!!!");
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
