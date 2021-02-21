a05b46a3c8d6353940d6c3a68823fbaa4ad269b7
Igor <75262127+Mazepin@users.noreply.github.com>
2021-02-22 00:13:52 +0300

    init: add Static constructor

---
+++ TestPr28 Static constructor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TestPr28_Static_constructor
+{
+    // Static constructor
+
+    class DbRepository
+    {
+        private static string connectionString;
+
+        static DbRepository() // создаем статический конструктор который вызовется только один раз
+        {
+            ConfigurationManager configurationManager = new ConfigurationManager();
+            connectionString = configurationManager.GetConnectionString();
+        }
+
+        public void GetData()
+        {
+            Console.WriteLine("Used: " + connectionString);
+        }
+    }
+
+    class ConfigurationManager
+    {
+        public string GetConnectionString()
+        {
+            return "local DB";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbRepository dbRepository = new DbRepository();
+
+            dbRepository.GetData();
+
+            Console.ReadKey();
+        }
+    }
+}
