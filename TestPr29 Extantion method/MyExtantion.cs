72424302cbad506aba250f6f884dd19b3b728f35
Igor <75262127+Mazepin@users.noreply.github.com>
2021-02-23 00:04:36 +0300

    init: add Extantion method

---
+++ TestPr29 Extantion method/MyExtantion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LessonMyExtantion  // создали пространство имен 
+{
+    // создаем extantion method
+    // обязательно static
+    // ключевое слово this 
+
+    static class MyExtantion
+    {
+        public static void Print(this DateTime dateTime)
+        {
+            Console.WriteLine(dateTime);
+        }
+
+        public static bool IsDayOfWeek(this DateTime dateTime, DayOfWeek dayOfWeek)
+        {
+            return dateTime.DayOfWeek == dayOfWeek;
+        }
+    }
+}
---
+++ TestPr29 Extantion method/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using LessonMyExtantion; // подключили пространство имен
+
+namespace TestPr29_Extantion_method
+{
+    class Program
+    {
+        // Extantion method
+        // обязательно static
+        // ключевое слово this
+
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;
+
+            currentDateTime.Print();
+
+            Console.WriteLine(currentDateTime.IsDayOfWeek(DayOfWeek.Sunday)); // проверка если текукщий день недели sunday то true 
+
+            // проверяем на день недели и выполняем какой то код
+
+            if (currentDateTime.IsDayOfWeek(DayOfWeek.Saturday) | currentDateTime.IsDayOfWeek(DayOfWeek.Sunday))
+            {
+                Console.WriteLine("Today is a day off!  You can drink beer!!!");
+            }
+            else 
+            {
+                Console.WriteLine("Today is a weekday! Go undress and work!!!");
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
