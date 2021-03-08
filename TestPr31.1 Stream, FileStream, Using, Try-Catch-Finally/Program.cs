923204aee053a115243b1086975cc676b8478ccd
Igor <75262127+Mazepin@users.noreply.github.com>
2021-03-08 14:42:38 +0300

    init: add DateTime and Homework StreamWrite, Using

---
+++ TestPr31.1 Stream, FileStream, Using, Try-Catch-Finally/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace TestPr31._1_Stream__FileStream__Using__Try_Catch_Finally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var sw = new StreamWriter("document.txt", true, Encoding.UTF8))
+            {
+                Console.WriteLine("Enter your Name: ");
+                string name = Console.ReadLine();
+                sw.WriteLine(name);
+
+                Console.WriteLine("Enter your Last Name: ");
+                string lastName = Console.ReadLine();
+                sw.WriteLine(lastName);
+
+                Console.WriteLine("Enter your Date of Birthday (dd-mm-yyyy): ");
+                DateTime dayBirthday1 = new DateTime();
+                var dayBirthday = DateTime.TryParse(Console.ReadLine(), out dayBirthday1);
+                sw.WriteLine(dayBirthday1);
+
+                Console.ReadLine();
+
+
+
+
+            }
+        }
+    }
+}
